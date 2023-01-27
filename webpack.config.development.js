3158c3144143c9f6af4c723a8f34009f1044f4cf
Sintija Linuza <sintija.linuza@gmail.com>
2023-01-27 00:02:35 +0000

    updated backend node functions and jason files

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,20 +1,20 @@
-const { merge } = require("webpack-merge");
-const path = require("path");
+const { merge } = require('webpack-merge')
+const path = require('path')
 
-const config = require("./webpack.config");
+const config = require('./webpack.config')
 
 module.exports = merge(config, {
-  mode: "development",
+  mode: 'development',
 
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
 
   devServer: {
     devMiddleware: {
-      writeToDisk: true,
-    },
+      writeToDisk: true
+    }
   },
 
   output: {
-    path: path.resolve(__dirname, "public"),
-  },
-});
+    path: path.resolve(__dirname, 'public')
+  }
+})
